0deaba6c8fd7dcae1ae2a550ed4bc34d59fcc250
Tsholofelo <tgnkuna768@gmail.com>
2025-01-09 09:43:51 +0200

    Add "ResponseDto" and "TestResultDto"

---
+++ Lab.BusinessLogicLayer/Models/DataTransferObjects/ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.BusinessLogicLayer.Models.DataTransferObjects
+{
+    public class ResponseDto<TData>
+    {
+        public TData? Data { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public IEnumerable<string> Errors { get; set; } = Enumerable.Empty<string>();
+    }
+}
---
+++ Lab.BusinessLogicLayer/Models/DataTransferObjects/TestResultDto.cs
@@ -0,0 +1,16 @@
+﻿using Lab.BusinessLogicLayer.Models.DataTransferObjects.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.BusinessLogicLayer.Models.DataTransferObjects
+{
+    public class TestResultDto : DtoBase<int>
+    {
+        public int TestId { get; set; }
+        public string Result { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+    }
+}
